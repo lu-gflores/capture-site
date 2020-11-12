fdeba37fc33c76983ec35f0c37a44e05b7c9d39f
George Flores <georgeflores886@gmail.com>
2020-11-12 11:38:37 -0500

    created styles for global

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import styled from 'styled-components'
+
 const AboutSection = () => {
     return (
-        <div>
+        <About>
             <div className="description">
                 <div className="title">
                 
@@ -24,8 +26,20 @@ const AboutSection = () => {
             <div className="image">
                 <img src={home1} alt="man with a camera"/>
             </div>
-        </div>
+        </About>
     )
 }
+
+//styled component
+const About = styled.div`
+
+min-height: 90vh;
+display: flex;
+align-items:center;
+justify-content: space-between;
+padding: 5rem 10rem;
+color:white;
+`
+
 
 export default AboutSection
