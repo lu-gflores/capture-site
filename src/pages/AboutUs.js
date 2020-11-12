63029453af659736f830cd7246110982cf071f79
George Flores <georgeflores886@gmail.com>
2020-11-12 11:20:51 -0500

    created FaqSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 const AboutUs = props => {
     return (
-<>
+    <>
         <AboutSection />
         <ServicesSection />
-</>
+        <FaqSection />
+    </>
     )
 }
 
