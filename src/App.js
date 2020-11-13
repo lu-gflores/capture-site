08be103330c76dec2ab1288a51d80156afe937f4
George Flores <georgeflores886@gmail.com>
2020-11-13 09:38:18 -0500

    added switch router for pages and create contact and work pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react'
 import AboutUs from './pages/AboutUs'
 import GlobalStyle from './components/GlobalStyle'
 import Nav from './components/Nav'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
     <GlobalStyle />
     <Nav />
-     <AboutUs />
+    <Switch>
+      <Route path='/' exact>
+        <AboutUs />
+      </Route>
+      <Route path='/work' exact>
+        <OurWork />
+      </Route>
+      <Route path='/contact' exact>
+        <ContactUs/>
+      </Route>
+    </Switch>
     </div>
   );
 }
