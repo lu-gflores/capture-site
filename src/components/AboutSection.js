15bbd3a4fbbc3b53a576a9eb1cbeec0cc6b0b50f
George Flores <georgeflores886@gmail.com>
2020-11-13 08:49:46 -0500

    added style components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,27 +5,27 @@ import styled from 'styled-components'
 const AboutSection = () => {
     return (
         <About>
-            <div className="description">
+            <Description>
                 <div className="title">
                 
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
+                    </Hide>
 
-                    <div className="hide">
+                    <Hide>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
+                    </Hide>
 
-                    <div className="hide">
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                     <p>Contact us for any photography or videos ideas that you have. We have professionals with awesome skills!</p>
                     <button>Contact Us</button>
                 </div>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="man with a camera"/>
-            </div>
+            </Image>
         </About>
     )
 }
@@ -39,7 +39,28 @@ align-items:center;
 justify-content: space-between;
 padding: 5rem 10rem;
 color:white;
+`;
+
+const Description = styled.div`
+flex: 1;
+padding-right: 5rem;
+
+h2 {
+font-weight: lighter;
+}
+`;
+
+const Image = styled.div`
+flex: 1;
+overflow: hidden;
+img{
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+}
+`;
+
+const Hide = styled.div`
+overflow:hidden;
 `
-
-
 export default AboutSection
